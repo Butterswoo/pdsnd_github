dc3721969afb8cd40047e507c56c7e54f2e3bc19
butters <diiwu@umich.edu>
2020-05-04 22:05:58 -0400

    Edit docstrings for user_status function

--- bikeshare.py
+++ bikeshare.py
@@ -134,7 +134,7 @@ def trip_duration_stats(df):
     print('-'*40)
     
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users input."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
